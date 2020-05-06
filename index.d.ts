0634772e0aac8c8303e0112b96748d1cb127df2c
wangchao <chao507@vip.qq.com>
2020-05-06 17:24:41 +0800

    tool types

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,10 @@ import * as Koa from "koa";
 import * as Knex from "knex";
 import * as KoaBouncer from "koa-bouncer";
 
-type Error<T = any> = { [code: string]: T; };
+interface ErrorOption {
+  code?: 500,
+  message?: ''
+}
 
 declare namespace cf {
   export class App extends Koa {
@@ -14,15 +17,23 @@ declare namespace cf {
     DB: Knex;
     service(name: string): Service;
     success(r: any): any;
-    error(r: Error): any;
+    error(r?: ErrorOption): any;
     getBody(name: string): KoaBouncer.Validator;
     getQuery(name: string): KoaBouncer.Validator;
   }
   
   export class Service {
     constructor(ctx: Koa.Context);
-    cursor: Knex.Table;
+    cursor: Knex.QueryBuilder;
     service(name: string): Service;
+  }
+
+  export namespace Tool {
+    function ts(): number;
+    function authcode(str: string, salt: string): string;
+    function arr2Obj(arr: array, identifier: string): object;
+    function isNumber(value: string): boolean;
+    function sleep(milliseconds: number): Promise<any>;
   }
 }
 
