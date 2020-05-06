09c48c152098484d798af0437c8c9a313b09f3f9
wangchao <chao507@vip.qq.com>
2020-05-06 16:47:53 +0800

    types

--- index.d.ts
+++ index.d.ts
@@ -4,22 +4,26 @@ import * as KoaBouncer from "koa-bouncer";
 
 type Error<T = any> = { [code: string]: T; };
 
-export interface App extends Koa {
-  run(port: number): void
+declare namespace cf {
+  export interface App extends Koa {
+    run(port: number): void
+  }
+  
+  export interface Controller {
+    constructor(ctx: Koa.Context, next: Koa.Next);
+    DB: Knex;
+    service(name: string): Service;
+    success(r: any): any;
+    error(r: Error): any;
+    getBody(name: string): KoaBouncer.Validator;
+    getQuery(name: string): KoaBouncer.Validator;
+  }
+  
+  export interface Service {
+    constructor(ctx: Koa.Context);
+    cursor: Knex.Table;
+    service(name: string): Service;
+  }
 }
 
-export interface Controller {
-  constructor(ctx: Koa.Context, next: Koa.Next);
-  DB: Knex;
-  service(name: string): Service;
-  success(r: any): any;
-  error(r: Error): any;
-  getBody(name: string): KoaBouncer.Validator;
-  getQuery(name: string): KoaBouncer.Validator;
-}
-
-export interface Service {
-  constructor(ctx: Koa.Context);
-  cursor: Knex.Table;
-  service(name: string): Service;
-}
+export = cf;
