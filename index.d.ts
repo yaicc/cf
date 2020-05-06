a0689dbc91230d059d36e1046254026cc015727f
wangchao <chao507@vip.qq.com>
2020-05-06 17:38:56 +0800

    description

--- index.d.ts
+++ index.d.ts
@@ -8,31 +8,103 @@ interface ErrorOption {
 }
 
 declare namespace cf {
+
+  /**
+   * Application entry extend Koa.
+   */
   export class App extends Koa {
+    /**
+     * Start the Application
+     * @param port port to listen
+     */
     run(port: number): void
   }
   
+  /**
+   * Base controller
+   */
   export class Controller {
     constructor(ctx: Koa.Context, next: Koa.Next);
+    /**
+     * Database instance
+     */
     DB: Knex;
+    /**
+     * Fetch service of name
+     * @param name serveice name
+     */
     service(name: string): Service;
-    success(r: any): any;
-    error(r?: ErrorOption): any;
+    /**
+     * Return success messgae with data
+     * @param data
+     */
+    success(data: any): any;
+    /**
+     * Return error messgae with code & msg
+     * @param data
+     */
+    error(error?: ErrorOption): any;
+    /**
+     * Fetch param with name from body
+     * @param name param name
+     */
     getBody(name: string): KoaBouncer.Validator;
+    /**
+     * Fetch param with name from query
+     * @param name param name
+     */
     getQuery(name: string): KoaBouncer.Validator;
   }
   
+  /**
+   * Base service
+   */
   export class Service {
     constructor(ctx: Koa.Context);
+    /**
+     * Database instance
+     */
+    DB: Knex;
+    /**
+     * Cursor of service talbe
+     */
     cursor: Knex.QueryBuilder;
+    /**
+     * Fetch service of name
+     * @param name serveice name
+     */
     service(name: string): Service;
   }
 
+  /**
+   * Common tool of framework
+   */
   export namespace Tool {
+    /**
+     * Linux timestamp
+     */
     function ts(): number;
+    /**
+     * Encode string with salt
+     * @param str string to encode
+     * @param salt 
+     */
     function authcode(str: string, salt: string): string;
+    /**
+     * Convert an array of objects to a single object
+     * @param arr 
+     * @param identifier 
+     */
     function arr2Obj(arr: array, identifier: string): object;
+    /**
+     * Check if a value is a number
+     * @param value 
+     */
     function isNumber(value: string): boolean;
+    /**
+     * Wait for an amount of milliseconds
+     * @param milliseconds 
+     */
     function sleep(milliseconds: number): Promise<any>;
   }
 }
